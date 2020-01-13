26ffd06f5c2705606fc15731529fa9689f6933e1
amm90 <amm90@mail.aub.edu>
2020-01-13 19:57:42 +0200

    populated hadith_ngram_lists_validator.py

---
+++ hadith_ngram_lists_validator.py
@@ -0,0 +1,53 @@
+import json
+import os
+
+import hadith_predictor as predictor
+
+
+def validate_sanad_maten_lists(sanad_bigrams_path, sanad_unigrams_path,
+                               maten_bigrams_path, maten_unigrams_path,
+                               test_set, min_tolerance=0, max_tolerance=5,
+                               output_path=None):
+    gold_split_positions = []
+    predicted_split_positions = []
+
+    for observation in test_set:
+        hadith = observation[0]
+        gold_split_position = observation[2]
+        gold_split_positions.append(int(gold_split_position))
+        predicted_split_position, _ = predictor.segment_hadith(hadith, sanad_bigrams_path, sanad_unigrams_path,
+                                                               maten_bigrams_path, maten_unigrams_path,
+                                                               predictor.PREDICTOR_INFORMATION_GAIN,
+                                                               )
+
+        predicted_split_positions.append(int(predicted_split_position))
+
+    accuracies = _get_accuracy_vary_tolerance(gold_split_positions, predicted_split_positions,
+                                              min_tolerance=min_tolerance, max_tolerance=max_tolerance)
+
+    if output_path:
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        fs = open(output_path + "accuracies.json", "w")
+        json.dump(fs, accuracies)
+
+    return accuracies
+
+
+def _get_accuracy_vary_tolerance(gold_splits, predicted_splits, min_tolerance=0, max_tolerance=5):
+    accuracies = {}
+    for tolerance in range(min_tolerance, max_tolerance):
+        accuracies[tolerance] = _split_position_accuracy(gold_splits, predicted_splits, tolerance)
+    return accuracies
+
+
+def _split_position_accuracy(gold_splits, predicted_splits, tolerance=3):
+    correctly_segmented = []
+
+    for (g, p) in zip(gold_splits, predicted_splits):
+        if abs(g - p) <= tolerance:
+            correctly_segmented.append(1)
+        else:
+            correctly_segmented.append(0)
+
+    return len([res for res in correctly_segmented if res == 1]) / len(correctly_segmented) * 100
